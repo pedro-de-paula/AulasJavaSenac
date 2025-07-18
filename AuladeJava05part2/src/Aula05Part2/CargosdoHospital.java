669fe5087fc3d5526624819af44fc0d4061764d6
PEDRO DE PAULA CAMARGO ARAUJO <pedro.pcaraujo@senacsp.edu.br>
2025-07-18 16:45:47 -0300

    Updated Projects

---
+++ AuladeJava05part2/src/Aula05Part2/CargosdoHospital.java
@@ -0,0 +1,23 @@
+package Aula05Part2;
+
+//Pedor de Paula Camargo Araújo
+class CargosdoHospital {
+
+    static class Medico {
+        public String getCargo() {
+            return "Médico";
+        }
+    }
+
+    static class Enfermeiro {
+        public String getCargo() {
+            return "Enfermeiro";
+        }
+    }
+
+    static class Paciente {
+        public String getCargo() {
+            return "Paciente";
+        }
+    }
+}
